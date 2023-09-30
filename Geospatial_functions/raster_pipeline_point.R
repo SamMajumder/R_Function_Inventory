f1282f1a19cff8dee03f0bd4b3e432846a463d60
Sam Majumder <sammajumder692@gmail.com>
2023-09-30 15:01:30 -0400

    raster_pipeline_point commit (rectifying a mistake)

--- Geospatial_functions/raster_pipeline_point.R
+++ Geospatial_functions/raster_pipeline_point.R
@@ -1,4 +1,10 @@
 
+rm(list = ls())
+
+library(sf)
+library(stars)
+library(purrr)
+library(tidyverse)
 
 raster_pipeline_point <- function(inputs, 
                                   df = NULL, 
@@ -12,14 +18,17 @@ raster_pipeline_point <- function(inputs,
                                   no_data_value = -9999,
                                   reference_shape = NULL) {
   
-  # Internal function to process a single raster 
+  # Initialize the progress bar
+  pb <- txtProgressBar(min = 0, max = length(inputs), style = 3)
+  
   process_raster <- function(input) {
-    # Check if the file exists
+    # Update progress bar
+    setTxtProgressBar(pb, which(inputs == input))
+    
     if (!file.exists(input)) {
       stop(paste("Error: The file", input, "does not exist."))
     }
     
-    # Try reading the raster
     raster_layer <- tryCatch({
       if (is.character(input)) {
         stars::read_stars(input)
@@ -29,6 +38,18 @@ raster_pipeline_point <- function(inputs,
     }, error = function(e) {
       stop(paste("Error reading the raster file:", input, "\nMessage:", e$message))
     })
+    
+    if (!is.null(reference_shape)) {
+      if (is.character(reference_shape)) {
+        reference_shape <- st_read(reference_shape, quiet = TRUE)
+      }
+      
+      if (!st_crs(reference_shape) == st_crs(raster_layer)) {
+        reference_shape <- st_transform(reference_shape, crs = st_crs(raster_layer))
+      }
+      
+      raster_layer <- st_crop(raster_layer, reference_shape)
+    }
     
     if (!is.null(resample_factor)) {
       new_dims <- map_dbl(dim(raster_layer), ~round(.x * resample_factor))
@@ -43,15 +64,15 @@ raster_pipeline_point <- function(inputs,
     return(raster_layer)
   }
   
-  # Apply the process_raster function to each input using map
   processed_rasters <- map(inputs, process_raster)
+  
+  # Close the progress bar
+  close(pb)
   
   dataframes_with_values <- list()
   
-  # If df, lat_col, and lon_col are provided
   if (!is.null(df) && !is.null(lat_col) && !is.null(lon_col)) {
     if (is.null(split_id) || is.null(search_strings)) {
-      # Extract raster values based on coordinates and order of rasters
       df_list <- list(df)
       for (file in inputs) {
         cat("Processing file:", file, "\n")
@@ -67,32 +88,26 @@ raster_pipeline_point <- function(inputs,
       }
       dataframes_with_values <- list(df)
     } else {
-      # Check if split_id column exists in the dataframe
       if (!split_id %in% colnames(df)) {
         stop(paste("Error: The column", split_id, "does not exist in the dataframe."))
       }
-      # Split the dataframe by split_id
-      df_list <- df %>% group_by(!!sym(split_id)) %>% group_split()
-      
+      df_list <- df %>% group_by(!!sym(split_id)) %>% dplyr::group_split()
       dataframes_with_values <- map(df_list, function(current_df) {
         if (nrow(current_df) == 0 || is.null(current_df[[split_id]])) {
           return(NULL)
         }
         current_id <- unique(current_df[[split_id]])
-        current_search_string <- str_c("-", current_id, "-")
-        
+        current_search_string <- paste0("-", current_id, "-")
         cat("Processing for ID:", current_id, "\n")
         
         if (current_search_string %in% search_strings) {
           files <- list.files(path = dirname(inputs[1]), 
-                              pattern = str_c(current_search_string, ".*\\.tif$"), 
+                              pattern = paste0(current_search_string, ".*\\.tif$"), 
                               full.names = TRUE)
-          
           if (length(files) == 0) {
             cat("No raster files found matching the search string:", current_search_string, "\n")
             return(NULL)
           }
-          
           for (file in files) {
             cat("Processing file:", file, "\n")
             tryCatch({
@@ -114,13 +129,37 @@ raster_pipeline_point <- function(inputs,
   return(list(processed_rasters = processed_rasters, dataframes_with_values = dataframes_with_values))
 }
 
+library(here)
+
+
+## TEST ## 
+
+raster_files <- list.files(here("Datasets"),
+                           pattern = ".tif",
+                           full.names = TRUE)
+
+
+years <- c("2000","2001","2002")
+
+df <- read.csv(here("Datasets","Kenya_conflicts.csv")) %>% 
+                                 dplyr::filter(YEAR %in% years)
 
 
 
 
-
-
-
+results <- raster_pipeline_point(
+  inputs = raster_files,
+  df = df,
+  lat_col = "LATITUDE",
+  lon_col = "LONGITUDE",
+  split_id = "YEAR",
+  search_strings = c("-2000-", "-2001-","2002"),
+  reference_shape = here("Datasets",
+                         "ken_adm_iebc_20191031_shp",
+                         "ken_admbnda_adm0_iebc_20191031.shp"),
+  resample_factor = 0.5,
+  crs = st_crs(4326)  
+)
 
 
 
